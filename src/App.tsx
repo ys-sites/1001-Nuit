777d0f9374ba4e5bba8600876c67cdcfd7d0f707
Sharafath <ys.mkg.sites@gmail.com>
2026-09-09 14:39:44 -0400

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import HomePage from './HomePage';
 import ReviewPage from './ReviewPage';
 import ClosedPage from './ClosedPage';
@@ -15,6 +16,7 @@ export default function App() {
           <Route path="/thank-you" element={<ThankYouPage />} />
         </Routes>
       </main>
+      <Analytics />
     </div>
   );
 }
